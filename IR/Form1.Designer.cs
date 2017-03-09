ea8098e17b3032ce7da46a3780c4b596988a93b4
amany helmy <amany.helmy03@gmail.com>
2017-03-09 22:27:40 +0200

    Add project files.

---
+++ IR/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace IR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crawl = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // crawl
+            // 
+            this.crawl.Location = new System.Drawing.Point(12, 12);
+            this.crawl.Name = "crawl";
+            this.crawl.Size = new System.Drawing.Size(75, 23);
+            this.crawl.TabIndex = 0;
+            this.crawl.Text = "Crawl";
+            this.crawl.UseVisualStyleBackColor = true;
+            this.crawl.Click += new System.EventHandler(this.crawl_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(276, 482);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(303, 41);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(928, 482);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 535);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.crawl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button crawl;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
+
---
+++ IR/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace IR
+{
+    public partial class Form1 : Form
+    {
+        private List<String> toVisit;
+        private List<String> visited;//urls
+        private List<String> content;//htmlcontent
+        public Form1()
+        {
+            InitializeComponent();
+            toVisit = new List<string>();
+            visited = new List<string>();
+            content = new List<string>();
+            toVisit.Add("http://www.google.com");
+            /*
+            string connectionString = "Data source=orcl; User Id=scott; Password=tiger;";
+            OracleConnection conn;
+            */
+        }
+
+        private void crawl_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (visited.Count != 3000)//طبعا محدش يرن 3000 الاب  هيوقف  :D 
+            {
+                if (toVisit.Count != 0)
+                {
+                    content.Add(HTTPRequest(toVisit[i]));
+                    visited.Add(toVisit[i]);
+                    listView1.Items.Add(new ListViewItem(toVisit[i]));
+                    listView2.Items.Add(new ListViewItem(content[i]));
+                    toVisit.RemoveAt(i);
+                    searchForLinks(content[i]);
+
+                    i++;
+                }
+            }
+        }
+
+        public string HTTPRequest(String URL)
+        {
+            
+            WebRequest myWebRequest;
+            WebResponse myWebResponse;
+
+            // Create a new 'WebRequest' object to the mentioned URL.
+            myWebRequest = WebRequest.Create(URL);
+
+            // The response object of 'WebRequest' is assigned to a WebResponse' variable.
+            myWebResponse = myWebRequest.GetResponse();
+
+            Stream streamResponse = myWebResponse.GetResponseStream();
+            StreamReader sReader = new StreamReader(streamResponse);
+            string rString = sReader.ReadToEnd();
+
+            streamResponse.Close();
+            sReader.Close();
+            myWebResponse.Close();
+            return rString;
+
+        }
+        public void searchForLinks(String content)
+        {
+            var urlDictionary = new Dictionary<string, string>();
+
+            Match match = Regex.Match(content, "(?i)<a .*?href=\"([^\"]+)\"[^>]*>(.*?)</a>");
+            while (match.Success)
+            {
+                
+                string urlKey = match.Groups[1].Value;
+
+                
+                string urlValue = Regex.Replace(match.Groups[2].Value, "(?i)<.*?>", string.Empty);
+
+                urlDictionary[urlKey] = urlValue;
+                match = match.NextMatch();
+            }
+
+            foreach (var item in urlDictionary)
+            {
+                string href = item.Key;
+                string text = item.Value;
+
+                if (!string.IsNullOrEmpty(href))
+                {
+                    string url = href.Replace("%3f", "?")
+                        .Replace("%3d", "=")
+                        .Replace("%2f", "/")
+                        .Replace("&amp;", "&");
+
+                    if (string.IsNullOrEmpty(url) || url.StartsWith("#")
+                        || url.StartsWith("mailto:", StringComparison.OrdinalIgnoreCase)
+                        || url.StartsWith("javascript:", StringComparison.OrdinalIgnoreCase))
+                    {
+                        continue;
+                    }
+
+                    if (url.Contains("http://")|| url.Contains("https://"))
+                    {
+                        toVisit.Add(url);
+                    }
+                    
+                }
+            }
+        }
+        /*
+        private void addCrawlerResultsToDatabase()
+        {
+            conn = new OracleConnection(connectionString);
+            conn.Open();
+            int count = 0;
+            OracleCommand cmd;
+
+            while (count < 3000)
+            {
+                cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT_NEW_CRAWLER_RESULT";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("page_url", OracleDbType.Varchar2, DBNull.Value, ParameterDirection.Input).Value = listView1.GetItemAt(0, count);
+                cmd.Parameters.Add("page_content", OracleDbType.Varchar2, DBNull.Value, ParameterDirection.Input).Value = listView2.GetItemAt(2, count);
+                cmd.ExecuteNonQuery();
+                count++;
+            }
+            */
+    }
+}
+
+
+
